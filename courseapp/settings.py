553dd1749e382122a1634c2d35130b1f2ea2d746
pradhumnts <62463488+pradhumnts@users.noreply.github.com>
2022-04-17 09:03:29 +0530

    static

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -119,10 +119,12 @@ import os
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# if DEBUG:
+#   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# else:
+#   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
